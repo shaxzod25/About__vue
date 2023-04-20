d0829d9b2a24cf45dd744e91e7bd2b788b4a8e95
shaxi <nigmatov2508@gmail.com>
2023-04-20 16:53:48 +0500

    aboutvue

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+    <footer class="footer">
+        <div class="container">
+            <h2 class="footer__h2">Связаться со мной</h2>
+            <ul class="footer_menu">
+                <li class="footer_item"><a class="footer_link" href="#"><img src="../assets/img/link1.png"></a></li>
+                <li class="footer_item"><a class="footer_link" href="#"><img src="../assets/img/link2.png"></a></li>
+                <li class="footer_item"><a class="footer_link" href="#"><img src="../assets/img/link3.png"></a></li>
+                <li class="footer_item"><a class="footer_link" href="#"><img src="../assets/img/link4.png"></a></li>
+                <li class="footer_item"><a class="footer_link" href="#"><img src="../assets/img/link5.png"></a></li>
+            </ul>
+        </div>
+     </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer',
+    data() {
+        return {
+            
+        }
+    },
+};
+</script>
+
+
+<style></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+    <header>
+        <div class="container">
+            <div class="header__wrap">
+                <a href="#" class="logo">PROWEB</a>
+                <ul class="menu">
+                    <li><a href="#" class="menu__link">Обо мне</a></li>
+                    <li><a href="#" class="menu__link">Мои работы</a></li>
+                    <li><a href="#" class="menu__link">Контакт</a></li>
+
+                </ul>
+            </div>
+        </div>
+    </header>
+    <section class="banner">
+        <div class="container"></div>
+        <div class="banner__wrap"> 
+            <h1 class="banner__heading">Добро пожаловать на мой сайт</h1>
+            <button class="banner__btn">Позвонить</button>
+        </div>
+    </section>
+</template>
+
+
+<script>
+export default {
+    name: 'NavbarTodo',
+    data() {
+        return {
+            
+        }
+    },
+};
+</script>
+
+<style></style>
